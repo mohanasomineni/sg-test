523d4b9c58ac5b5eba8b843ee0a7e4192160f37d
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-02-21 15:03:27 -0500

    sg test

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@
   }
 
   backend "s3" {
-    bucket = "mohana-remote-state"
+    bucket = "mohana-tf-remote-state"
     key    = "vpc-test" # you should have unique keys within the bucket, same key should not use in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "mohana-state-locking"
